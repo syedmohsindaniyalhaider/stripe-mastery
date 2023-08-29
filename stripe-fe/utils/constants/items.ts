5c26dc24c05be6d9c5a3943e949f66d8325a3fd2
syedmohsindaniyalhaider <syedmohsindaniyalhaider@gmail.com>
2023-08-29 19:11:09 +0500

    feat: all items

---
+++ stripe-fe/utils/constants/items.ts
@@ -0,0 +1,37 @@
+export const Cart = [
+  {
+    id: 1,
+    name: "New Item 1",
+    amount: 100,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    name: "New Item 2",
+    amount: 200,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 3,
+    name: "New Item 3",
+    amount: 300,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 4,
+    name: "New Item 4",
+    amount: 400,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 5,
+    name: "New Item 5",
+    amount: 500,
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+];
