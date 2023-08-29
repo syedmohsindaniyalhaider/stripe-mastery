b7b1e4476f846f48e6ea6a0f3821d8641a531c71
syedmohsindaniyalhaider <syedmohsindaniyalhaider@gmail.com>
2023-08-29 17:33:20 +0500

    feat: stripe create payment intent api

--- stripe-be/src/main.ts
+++ stripe-be/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerDocumentOptions,
+} from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Stripe Mastery')
+    .setDescription('Apis')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('/api/v1', app, document);
   app.enableCors();
   await app.listen(5555);
 }
